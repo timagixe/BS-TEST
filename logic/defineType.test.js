269393844a4dd2edada616a1d8f14004f8bdd9ea
Tima Gixe <timagixe@protonmail.com>
2020-08-25 13:48:56 +0300

    Update test results

--- logic/defineType.test.js
+++ logic/defineType.test.js
@@ -2,27 +2,30 @@ const _ = require('lodash');
 const { defineType } = require('./defineType');
 
 test('it should return string if string type', () => {
-    expect(defineType('string type')).toBe('string');
+    expect(defineType('string type')).toMatchObject({ type: 'string' });
 });
 
 test('it should return boolean if bool type', () => {
-    expect(defineType(true)).toBe('boolean');
+    expect(defineType(true)).toMatchObject({ type: 'boolean' });
 });
 
 test('it should return array if array type', () => {
-    expect(defineType(['first', 'second'])).toBe('array');
+    expect(defineType(['first', 'second'])).toMatchObject({ type: 'array' });
 });
 
 test('it should return number if number type', () => {
-    expect(defineType(5)).toBe('number');
+    expect(defineType(5)).toMatchObject({ type: 'number' });
 });
 
 test('it should return object if empty object type', () => {
-    expect(defineType({})).toBe('object');
+    expect(defineType({})).toMatchObject({ type: 'object' });
 });
 
 test('it should return object with props if object type', () => {
     expect(defineType({ propName: 'propValue' })).toMatchObject({
-        propName: { type: 'string' },
+        type: 'object',
+        properties: {
+            propName: { type: 'string' },
+        },
     });
 });
